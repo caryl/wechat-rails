387534b21cfd661744dd4a8ea23bc15791fefb1c
王宪伟 <caryl@users.noreply.github.com>
2015-05-13 11:27:05 +0800

    Update message.rb
    兼容1.9.3语法

--- lib/wechat/message.rb
+++ lib/wechat/message.rb
@@ -25,7 +25,7 @@ module Wechat
         @items=Array.new
       end
 
-      def item title: "title", description = nil, pic_url = nil, url: nil
+      def item title: "title", description = nil, pic_url = nil, url = nil
         items << {:Title=> title, :Description=> description, :PicUrl=> pic_url, :Url=> url}.reject{|k,v| v.nil? }
       end
     end
